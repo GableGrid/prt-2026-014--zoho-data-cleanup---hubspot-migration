973e0440aab6ad96f1599de277f644393c82a952
Murad <murad@gablegrid.com>
2026-04-18 18:34:48 +0600

    SETUP: Tython file save

--- src/last.py
+++ src/last.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import os
 
-xls = pd.ExcelFile("contacts.xlsx")
+# Get the directory where this script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "CrossRef_DataHealth_Report.xlsx")
+
+xls = pd.ExcelFile(file_path)
 print(xls.sheet_names)
